0bbf7d1dfa617cf319530c8e38838897f55b497f
Stephen T. Cannon <stephentcannon@gmail.com>
2012-05-22 06:48:41 -0400

    latest

--- contact_us/server/server-contactus.js
+++ contact_us/server/server-contactus.js
@@ -19,7 +19,6 @@ if (Meteor.is_server) {
   if (base == '/'){
     base = path.dirname(global.require.main.filename);   
   }
-
   var email;
   var emailPath = 'node_modules/nodemailer';
   var publicEmailPath = path.resolve(base+'/public/'+emailPath);
@@ -107,7 +106,7 @@ if (Meteor.is_server) {
         );
         sendEmail('Demo Contact Form Submission: ' + params.subject,
           'Comments\n' + params.comments,
-          '<html><p>From: '+params.name+'<'+params.email+'></p><p>Subject: '+params.subject+'</p><p>Comments:</p>'+params.comments+'</html>',
+          '<html><p>From: '+params.name+'</p><p>Email: '+params.email+'</p><p>Subject: '+params.subject+'</p><p>Comments:</p>'+params.comments+'</html>',
           config.email_to,
           params.name+'<'+params.email+'>'
         );
@@ -120,59 +119,3 @@ if (Meteor.is_server) {
     }
   }
 }
-
-if (Meteor.is_client) {
-
-  Meteor.startup(function () {
-    //placeholder if you want to code startup stuff
-  });
-
-  $(function() {
-    $("#name").focus();
-  });
-
-  Template.contactus.events = {
-    'click #btnContact': function (event) {
-      var params = $('#contact-form').toJSON();
-      try{
-        ContactUs.validateParams(params);
-        ContactUs.validateEmail(params.email);
-        $('#buttons-contact').fadeOut(1000, 'linear', function(){
-          $('#progress-contact').fadeIn(1000, 'linear', function(){
-           $('#bar-contact').width('66%');
-           Meteor.call('insertContactUs', params, function (error, result) { 
-              if(result){
-                $('#bar-contact').width('100%');
-                $('#progress-contact').fadeOut(1000, 'linear', function(){
-                  $('#bar-contact').width('33%');
-                  $('#contact-form').fadeOut(2000, 'linear', function(){
-                    $('#buttons-contact').show();
-                  });
-                  Alert.setAlert('Awesome!', result, 'alert-success', 'contact');
-                  $('#contactUsDialog').fadeOut(3000, 'linear', function(){
-                    $('#contactUsDialog').modal('hide');
-                    $('#contact-form').show();
-                    $("#contact-form").reset();
-                  }) 
-                });
-              } else {
-                $('#progress-contact').fadeOut(1000, 'linear', function(){
-                  $('#bar-contact').width('33%');
-                  if(error){
-                    Alert.setAlert('Error', error + error.reason, 'alert-error', 'contact');  
-                  } else {
-                    Alert.setAlert('Error', 'Unknown Error', 'alert-error', 'contact');
-                  }
-                  $('#buttons-contact').fadeIn(1000, 'linear');
-                });
-              }
-           });
-          });
-        }); 
-      } catch(error) {
-        Alert.setAlert('Error', error.reason, 'alert-error', 'contact');
-      }
-    }
-  };
-}
-
